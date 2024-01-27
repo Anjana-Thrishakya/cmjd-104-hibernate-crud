5af38e559d917e6672356f2c09283062766f0972
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-01-27 10:36:31 +0530

    Create simple entity

--- src/entity/StudentEntity.java
+++ src/entity/StudentEntity.java
@@ -1,5 +1,31 @@
 package entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "student")
 public class StudentEntity {
 
+    // ID attribute
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // Attribute
+    @Column(name = "nic", nullable = false, length = 12)
+    private String nic;
+
+    @CreationTimestamp
+    @Column(name = "create_date", nullable = false)
+    private Date createDate;
 }
