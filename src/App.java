dc3ec1b656dddc3ace4bbeaefde53ca3112652d7
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-01-27 12:06:04 +0530

    Student CRUD

--- src/App.java
+++ src/App.java
@@ -1,29 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Session;
 
 import entity.StudentEntity;
 import entity.embeded.StudentName;
 import repository.StudentRepository;
-import util.SessionFactoryConfiguration;
 
 public class App {
     public static void main(String[] args) throws Exception {
         StudentRepository studentRepository = new StudentRepository();
         
-        StudentName studentName = new StudentName("Student 1 First Name", "Student 1 Last Name");
+         StudentName studentName = new StudentName("Student 2 First Name", "Student 2 Last Name");
         
-        List<String> mobiles = new ArrayList<>();
-        mobiles.add("0776787789");
-        mobiles.add("0776787739");
+         List<String> mobiles = new ArrayList<>();
+         mobiles.add("0776787789");
+         mobiles.add("0776787739");
         
-        StudentEntity entity = new StudentEntity();
-        entity.setNic("943324149V");
-        entity.setName(studentName);
-        entity.setMobiles(mobiles);
+         StudentEntity entity = new StudentEntity();
+         entity.setNic("943324149V");
+         entity.setName(studentName);
+         entity.setMobiles(mobiles);
 
-        Integer id = studentRepository.saveStudent(entity);
-        System.out.println("Student Id : " + id);
+         Integer id = studentRepository.saveStudent(entity);
+         System.out.println("Student Id : " + id);
+
+        entity = studentRepository.getStudent(1);
+        System.out.println(entity.toString());
+        entity.setNic("954456789V");
+        studentRepository.updateStudent(entity);
+        StudentEntity entity2 = studentRepository.getStudent(1);
+        System.out.println(entity2.toString());
+
+        studentRepository.deleteStudent(entity2);
     }
 }
--- src/repository/StudentRepository.java
+++ src/repository/StudentRepository.java
@@ -9,7 +9,7 @@ import util.SessionFactoryConfiguration;
 public class StudentRepository {
     Session session = SessionFactoryConfiguration.getInstance().getSession();
 
-    public Integer saveStudent(StudentEntity entity){
+    public Integer saveStudent(StudentEntity entity) {
         Transaction transaction = session.beginTransaction();
 
         try {
@@ -19,6 +19,33 @@ public class StudentRepository {
         } catch (Exception e) {
             transaction.rollback();
             return -1;
+        }
+    }
+
+    public StudentEntity getStudent(Integer id) {
+        StudentEntity studentEntity = session.get(StudentEntity.class, id);
+        return studentEntity;
+    }
+
+    public void updateStudent(StudentEntity entity) {
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            session.update(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+    }
+
+    public void deleteStudent(StudentEntity entity) {
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            session.delete(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
         }
     }
 
